2aeaed73af05726f5ad2abac8b4f496dbbe12879
AqingCyan <xuekaiqi@creatshare.com>
2019-02-06 16:41:56 +0800

    :whale:插入排序

--- 排序算法/02_insert_sort.js
+++ 排序算法/02_insert_sort.js
@@ -1,4 +1,20 @@
 // 插入排序
 function insert(list) {
-
+    for (let j = 1; j < list.length; j++) {
+        let i = j;
+        while (i > 0) {
+            if (list[i] < list[i - 1]) {
+                list[i] = list[i] + list[i + 1];
+                list[i + 1] = list[i] - list[i + 1];
+                list[i] = list[i] - list[i + 1]
+            } else {
+                break
+            }
+        }
+    }
+    return list
 }
+
+// 测试用例
+let nums = [-12, -10, -10, -9, -8, -8, -7, -7, -6, -5, -4, -4, -4, -3, -1, 1, 2, 2, 3, 4, 5, 6, 6, 7, 8, 8, 9, 9, 10, 10, 10];
+console.log(insert(nums));
